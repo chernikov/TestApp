e60ded881f7ed4df09f7c0f298afc485272063d3
Andriy Chernikov <chernikov@gmail.com>
2021-04-07 13:27:20 +0300

    Add project files.

---
+++ TestApp.Tests/UnitTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace TestApp.Tests
+{
+    public class Tests
+    {
+        [TestCaseSource(nameof(FormatCases))]
+        public void FormatTests(int seconds, string answer)
+        {
+            var result = HumanTimeFormat.formatDuration(seconds);
+            Assert.AreEqual(answer, result);
+        }
+
+
+        static object[] FormatCases =
+        {
+            new object[] { 12, "12 seconds" },
+            new object[] { 120, "2 minutes" },
+            new object[] { 1120, "18 minutes and 40 seconds" },
+            new object[] { 594835, "6 days, 21 hours, 13 minutes and 55 seconds" }
+        };
+
+
+    }
+}
---
+++ TestApp/HumanTimeFormat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HumanTimeFormat
+{
+    public static string formatDuration(int seconds)
+    {
+        return null;
+    }
+}
